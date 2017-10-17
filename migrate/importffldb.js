a3b615c1391eb25e312627cad316769ea04258bb
Javed Khan <tuxcanfly@gmail.com>
2017-10-17 23:41:42 +0530

    WIP: import flat files

--- migrate/importffldb.js
+++ migrate/importffldb.js
@@ -1,29 +1,45 @@
 'use strict';
 
-const bcoin = require('../');
 const fs = require('fs');
 const assert = require('assert');
+const Block = require('../lib/primitives/block');
 const BlockIO = require('../lib/utils/blockio');
 const BlockStream = require('../lib/utils/blockstream');
+const Chain = require('../lib/blockchain/chain');
 
-const location = process.argv[2];
-assert(typeof location === 'string', 'Please pass in a blocks path.');
+let location = process.argv[2];
+const from = process.argv[3];
+assert(typeof location === 'string', 'Please pass in a database path.');
+assert(typeof from === 'string', 'Please pass in a blocks path.');
+
+location = location.replace(/\.fdb\/?$/, '');
+
+const chain = new Chain({
+  db: 'ffldb',
+  location: location,
+  network: 'simnet'
+});
 
 const blockio = BlockIO({
-  location: location,
-  maxFileSize: 512 * 1024 * 1024,
+  location: from,
   network: 'simnet'
 });
 
 const blockstream = new BlockStream({network: 'simnet'});
 
-blockstream.on('data', (chunk) => {
-  const b = bcoin.block.fromRaw(chunk);
-  console.log(b.rhash());
+blockstream.on('data', async (chunk) => {
+  const block = Block.fromRaw(chunk);
+  const hash = block.rhash();
+  try {
+    await chain.add(block);
+    console.log('imported block %s', hash);
+  } catch (e) {
+    console.warn('%s: ', e);
+  }
 });
 
 const end = new Promise((resolve, reject) => {
-    blockstream.on('end', () => resolve());
+    blockstream.on('close', () => resolve());
     blockstream.on('error', reject);
 });
 
@@ -38,10 +54,10 @@ async function importflatfiles() {
 };
 
 (async () => {
-  await blockio.open();
+  await chain.open();
   console.log('Opened %s.', location);
   await importflatfiles();
-  await blockio.close();
+  await chain.close();
 })().then(() => {
   console.log('Migration complete.');
   process.exit(0);
