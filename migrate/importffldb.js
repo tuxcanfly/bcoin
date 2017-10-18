e6528814908569d21de47774d09a367215e3ea50
Javed Khan <tuxcanfly@gmail.com>
2017-10-18 19:03:55 +0530

    blockstream: fix arg

--- migrate/importffldb.js
+++ migrate/importffldb.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const assert = require('assert');
+const networks = require('../lib/protocol/networks');
 const Block = require('../lib/primitives/block');
 const BlockIO = require('../lib/utils/blockio');
 const BlockStream = require('../lib/utils/blockstream');
@@ -14,18 +15,32 @@ assert(typeof from === 'string', 'Please pass in a blocks path.');
 
 location = location.replace(/\.fdb\/?$/, '');
 
+const options = {};
+options.spv = process.argv.indexOf('--spv') !== -1;
+options.prune = process.argv.indexOf('--prune') !== -1;
+options.indexTX = process.argv.indexOf('--index-tx') !== -1;
+options.indexAddress = process.argv.indexOf('--index-address') !== -1;
+options.network = networks.main;
+
+const index = process.argv.indexOf('--network');
+
+if (index !== -1) {
+  options.network = networks[process.argv[index + 1]];
+  assert(options.network, 'Invalid network.');
+}
+
 const chain = new Chain({
   db: 'ffldb',
   location: location,
-  network: 'simnet'
+  network: options.network.type
 });
 
 const blockio = BlockIO({
   location: from,
-  network: 'simnet'
+  network: options.network.type
 });
 
-const blockstream = new BlockStream({network: 'simnet'});
+const blockstream = new BlockStream({network: options.network.type});
 
 blockstream.on('data', async (chunk) => {
   const block = Block.fromRaw(chunk);
