df6d9764968478098de3a40d2fb24210b87e9ae4
Javed Khan <tuxcanfly@gmail.com>
2018-01-07 13:56:09 +0530

    test: fix gcs test

--- test/gcs-test.js
+++ test/gcs-test.js
@@ -9,7 +9,7 @@ const bcoin = require('../../bcoin');
 
 describe('Compact Filters', function() {
   it('should build basic filter for testnet3 genesis block}', () => {
-    const genesis = bcoin.block.fromRaw(
+    const genesis = bcoin.Block.fromRaw(
       bcoin.networks.testnet.genesisBlock, 'hex');
     const basic = GCSFilter.fromBlock(genesis);
     const expected = Buffer.from('84134c0da400', 'hex');
