f89a6328e7ef20a795e4215f9257294b4c83bb52
Javed Khan <tuxcanfly@gmail.com>
2018-01-14 17:09:17 +0530

    test: update gcs test

--- test/gcs-test.js
+++ test/gcs-test.js
@@ -8,11 +8,23 @@ const GCSFilter = require('golomb');
 const bcoin = require('../../bcoin');
 
 describe('Compact Filters', function() {
-  it('should build basic filter for testnet3 genesis block}', () => {
+  it('should build regular filter', () => {
     const genesis = bcoin.Block.fromRaw(
       bcoin.networks.testnet.genesisBlock, 'hex');
     const basic = GCSFilter.fromBlock(genesis);
     const expected = Buffer.from('84134c0da400', 'hex');
+    const hash = genesis.hash();
+    const key = hash.slice(0, 16);
+    basic.match(key, genesis.txs[0].hash());
     assert.bufferEqual(basic.data, expected);
+  });
+
+  it('should match tx against filter}', () => {
+    const genesis = bcoin.Block.fromRaw(
+      bcoin.networks.testnet.genesisBlock, 'hex');
+    const basic = GCSFilter.fromBlock(genesis);
+    const hash = genesis.hash();
+    const key = hash.slice(0, 16);
+    assert.ok(basic.match(key, genesis.txs[0].hash()));
   });
 });
