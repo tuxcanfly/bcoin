669e47a7ccfee1f249da7c839ae9a5982f256b89
Javed Khan <tuxcanfly@gmail.com>
2018-01-16 00:39:24 +0530

    tests: update gcs tests

--- test/gcs-test.js
+++ test/gcs-test.js
@@ -7,24 +7,23 @@ const assert = require('./util/assert');
 const GCSFilter = require('golomb');
 const bcoin = require('../../bcoin');
 
+const genesis = bcoin.Block.fromRaw(
+  bcoin.networks.testnet.genesisBlock, 'hex');
+const basic = GCSFilter.fromBlock(genesis);
+const hash = genesis.hash();
+const key = hash.slice(0, 16);
+
 describe('Compact Filters', function() {
   it('should build regular filter', () => {
-    const genesis = bcoin.Block.fromRaw(
-      bcoin.networks.testnet.genesisBlock, 'hex');
-    const basic = GCSFilter.fromBlock(genesis);
     const expected = Buffer.from('84134c0da400', 'hex');
-    const hash = genesis.hash();
-    const key = hash.slice(0, 16);
-    basic.match(key, genesis.txs[0].hash());
     assert.bufferEqual(basic.data, expected);
   });
 
-  it('should match tx against filter}', () => {
-    const genesis = bcoin.Block.fromRaw(
-      bcoin.networks.testnet.genesisBlock, 'hex');
-    const basic = GCSFilter.fromBlock(genesis);
-    const hash = genesis.hash();
-    const key = hash.slice(0, 16);
+  it('should match tx against filter', () => {
     assert.ok(basic.match(key, genesis.txs[0].hash()));
+  });
+
+  it('should fail missing tx on filter', () => {
+    assert.ok(!basic.match(key, Buffer.alloc(32)));
   });
 });
