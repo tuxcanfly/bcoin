d38db58f107408fe5a1d2656d51bf6015e9d586e
Javed Khan <tuxcanfly@gmail.com>
2018-04-21 07:25:42 +0530

    migrate: fix index migration

--- migrate/chaindb4to5.js
+++ migrate/chaindb4to5.js
@@ -1,17 +1,13 @@
 'use strict';
 
 const assert = require('assert');
-const bio = require('bufio');
 const bdb = require('bdb');
-const Network = require('../lib/protocol/network');
 const layout = require('../lib/blockchain/layout');
 
 // changes:
-// rm indexes
+// removes tx, addr indexes i.e layout.t, layout.T, layout.C
 
 assert(process.argv.length > 2, 'Please pass in a database path.');
-
-let parent = null;
 
 const db = bdb.create({
   location: process.argv[2],
@@ -21,65 +17,10 @@ const db = bdb.create({
   createIfMissing: false
 });
 
-/**
- * ChainFlags
- */
-
-class ChainFlags {
-  constructor(options) {
-    this.network = Network.primary;
-    this.spv = false;
-    this.witness = true;
-    this.prune = false;
-    this.indexTX = false;
-    this.indexAddress = false;
-    this.bip91 = false;
-    this.bip148 = false;
-
-    if (options)
-      this.fromOptions(options);
-  }
-
-  fromRaw(data) {
-    const br = bio.read(data);
-
-    this.network = Network.fromMagic(br.readU32());
-
-    const flags = br.readU32();
-
-    this.spv = (flags & 1) !== 0;
-    this.witness = (flags & 2) !== 0;
-    this.prune = (flags & 4) !== 0;
-    this.indexTX = (flags & 8) !== 0;
-    this.indexAddress = (flags & 16) !== 0;
-    this.bip91 = (flags & 32) !== 0;
-    this.bip148 = (flags & 64) !== 0;
-
-    return this;
-  }
-
-  static fromRaw(data) {
-    return new ChainFlags().fromRaw(data);
-  }
-}
-
-async function updateVersion() {
-  console.log('Checking version.');
-
-  await db.verify(layout.V.build(), 'chain', 4);
-
-  console.log('Updating version to %d.', 5);
-
-  await db.del(layout.V.build());
-  await db.verify(layout.V.build(), 'chain', 5);
-}
-
-async function removeKey(name, prefix) {
-  console.log('Removing %s index', name);
-
+async function removeKey(name, key) {
   const iter = db.iterator({
-    gte: prefix.min(),
-    lte: prefix.max(),
+    gte: key.min(),
+    lte: key.max(),
     reverse: true,
     keys: true
   });
@@ -89,7 +30,6 @@ async function removeKey(name, prefix) {
 
   while (await iter.next()) {
     const {key} = iter;
-
     batch.del(key);
 
     if (++total % 10000 === 0) {
@@ -98,7 +38,6 @@ async function removeKey(name, prefix) {
       batch = db.batch();
     }
   }
-
   await batch.write();
 
   console.log('Cleaned up %d %s index records.', total, name);
@@ -112,26 +51,23 @@ async function removeKey(name, prefix) {
   await db.open();
 
   console.log('Opened %s.', process.argv[2]);
+  console.log('Checking version.');
+  await db.verify(layout.V.build(), 'chain', 4);
 
-  await updateVersion();
+  const t = bdb.key('t', ['hash256']);
+  const T = bdb.key('T', ['hash', 'hash256']);
+  const C = bdb.key('C', ['hash', 'hash256', 'uint32']);
 
-  const data = await db.get(layout.O.build());
-  const flags = ChainFlags.fromRaw(data);
+  await removeKey('hash -> tx', t);
+  await removeKey('addr -> tx', T);
+  await removeKey('addr -> coin', C);
 
-  if (!flags.indexTX && !flags.indexAddress) {
-    await db.close();
-    return;
-  }
+  console.log('Compacting database...');
+  await db.compactRange();
 
-  parent = db.batch();
-  if (flags.indexTX)
-    removeKey('hash -> tx', layout.t);
-
-  if (flags.indexAddress) {
-    removeKey('addr -> tx', layout.T);
-    removeKey('addr -> coin', layout.C);
-  }
-  await parent.write();
+  console.log('Updating version to %d.', 5);
+  await db.del(layout.V.build());
+  await db.verify(layout.V.build(), 'chain', 5);
 
   await db.close();
 })().then(() => {
